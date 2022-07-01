919c94b03729445d3febb66e3490dc9558686c6a
ach <82474261+ach-git@users.noreply.github.com>
2022-07-01 20:48:05 +0200

    Forget code

--- config.lua
+++ config.lua
@@ -5,17 +5,3 @@ config = {
 
 	time = 10 -- temps en seconde
 }
-
-
-
-config.pos = { 
-	
-    Menu = {
-		position = {x = 1186.3, y = -1469.4, z = 35.1} -- x,y,z
-	}
-
-    
-} 
-
-
-config.time = 10 -- temps en seconde
